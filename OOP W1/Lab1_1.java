2b6c503b9eee2fb5fa0bf8472144e5ab55ca6b97
Lívia Guimarães <liviafgn31@gmail.com>
2026-03-12 01:53:22 +0000

    Refactor Lab1_1.java to remove author and date comments

--- OOP W1/Lab1_1.java
+++ OOP W1/Lab1_1.java
@@ -1,7 +1,5 @@
 /****************************************
  * Lab1_1 (Control): print out Hello World
- * Author: Livia Ferreira Guimaraes Neves
- * Date: 23rd September 2025
  ****************************************/
 package ie.tudublin.cmpu2016.lab1;
 
@@ -9,5 +7,4 @@ public class Lab1_1 {
 	public static void main(String[] args) {
 		System.out.println("Hello, Java!");
 	}
-
 }
