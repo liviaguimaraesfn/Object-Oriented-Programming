9b6c3366dbaf00a64f9e5c8b152b4811f3fea286
Lívia Guimarães <liviafgn31@gmail.com>
2026-03-12 01:53:08 +0000

    Refactor Control class comments and formatting

--- OOP W1/Control.java
+++ OOP W1/Control.java
@@ -1,7 +1,5 @@
 /****************************************
  * Control: the purpose of this class is to create module info
- * Author: Livia Ferreira Guimaraes Neves
- * Date: 23rd September 2025
  ****************************************/
 package ie.tudublin.cmpu2016.lab1;
 
@@ -9,5 +7,4 @@ public class Control {
 	public static void main(String[] args) {
 		System.out.println("First Lab");
 	}
-
 }
